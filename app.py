a9baa119ae4f11486a576d6326d1d960ec38795d
Sebastian Contreras <sacm1046@gmail.com>
2020-06-16 18:16:57 -0400

    pass

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = 'fromyourwebmessage@gmail.com'
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_PASSWORD'] = 'asd'
 jwt = JWTManager(app)
 db.init_app(app)
 mail = Mail(app)
