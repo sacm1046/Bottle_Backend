de7684d0a0131e319b24f2864a88049bc84eb543
sacm1046 <sacm1046@gmail.com>
2020-06-17 19:05:58 +0000

    gitignore1

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = 'fromyourwebmessage@gmail.com'
-app.config['MAIL_PASSWORD'] = 'mlwxjkdgweulpxfl'
+app.config['MAIL_PASSWORD'] = 'vrzrjjgfmadkbcey'
 jwt = JWTManager(app)
 db.init_app(app)
 mail = Mail(app)
