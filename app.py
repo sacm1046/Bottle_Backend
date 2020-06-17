3955034f804ebe6a6d90875d098f7f7b0cf25dbd
sacm1046 <sacm1046@gmail.com>
2020-06-17 20:45:20 +0000

    env1

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = 'fromyourwebmessage@gmail.com'
-app.config['MAIL_PASSWORD'] = 'vrzrjjgfmadkbcey'
+app.config['MAIL_PASSWORD'] = 'vkfaujqxuleqbrql'
 jwt = JWTManager(app)
 db.init_app(app)
 mail = Mail(app)
