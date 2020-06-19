2ed29b012eca1ffd4f90db772b9d74426b7da26d
Sebastian Contreras <sacm1046@gmail.com>
2020-06-18 20:39:24 -0400

    env2

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 jwt = JWTManager(app)
 db.init_app(app)
 mail = Mail(app)
